aec861ba431a6c9edf5c2e94ee7aa1b9a382e6dd
jzcdn <jzcdn@qq.com>
2017-10-11 11:50:36 +0800

    voypay addons prestashop1.6 index.php

--- voypay/index.php
+++ voypay/index.php
@@ -1,32 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: denny
- * Date: 2017/8/18
- * Time: 15:18
- */
-echo '<pre>';
-error_reporting(E_ALL);
-ini_set('display_errors','On');
-register_shutdown_function(function(){echo '<pre>';print_r(error_get_last());});
-try{
-    require 'sdk/voypay.php';
 
-    $config = array(
-        'mer_no' => '100001',
-        'accesskey'=>'9690178FC665A57521016F70A6D2F842',
-        'mode'=>'test',
-        'debug'=>false,
-    );
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
 
-    $param = array(
-        'voypay_trade_no'=>'1708021117083837896'
-    );
-    $m = new \voypay\refund($config);
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
 
-    $trade = $m->get($param);
-    print_r($trade);
-}
-catch (Exception $e){
-    print_r($e);
-}
+header('Location: ../');
+exit;
